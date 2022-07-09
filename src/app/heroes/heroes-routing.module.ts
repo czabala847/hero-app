8ca65af9c2ef2e258054638112b6f3f275442d12
Carlos Zabala <czabala847@gmail.com>
2022-07-09 17:03:16 -0500

    Instalando angular-flex

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddComponent } from './pages/add/add.component';
+import { HeroComponent } from './pages/hero/hero.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
 import { SearchComponent } from './pages/search/search.component';
@@ -8,6 +9,7 @@ import { SearchComponent } from './pages/search/search.component';
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {
         path: 'list',
@@ -27,7 +29,7 @@ const routes: Routes = [
       },
       {
         path: ':id',
-        component: HomeComponent,
+        component: HeroComponent,
       },
       {
         path: '**',
