a0eac6a06a646fa0d66c07a58fa2f5920d0e0738
Carlos Zabala <czabala847@gmail.com>
2022-07-09 16:09:02 -0500

    Creando rutas para el modulo auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
-
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
     path: '404',
-    component: ErrorPageComponent
+    component: ErrorPageComponent,
   },
   {
     path: '**',
-    redirectTo: '404'
-  }
-]
+    redirectTo: '404',
+  },
+];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
