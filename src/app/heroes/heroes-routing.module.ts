795f950a30833ee4469011cfdc3d8d70ae07c009
Carlos Zabala <czabala847@gmail.com>
2022-07-09 16:16:06 -0500

    Creando rutas para el modulo heroes

---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './pages/add/add.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
+import { SearchComponent } from './pages/search/search.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'list',
+        component: ListComponent,
+      },
+      {
+        path: 'add',
+        component: AddComponent,
+      },
+      {
+        path: 'edit/:id',
+        component: AddComponent,
+      },
+      {
+        path: 'search',
+        component: SearchComponent,
+      },
+      {
+        path: ':id',
+        component: HomeComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'list',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HeroesRoutingModule {}
