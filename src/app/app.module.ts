24ce140351895166df05954bb9aa1547b8931d0c
Carlos Zabala <czabala847@gmail.com>
2022-07-09 19:11:59 -0500

    Creando servicio para consultar heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,7 +9,12 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
   declarations: [AppComponent, ErrorPageComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
